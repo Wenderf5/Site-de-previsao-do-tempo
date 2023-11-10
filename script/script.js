18bff161a9e48d1e387f83421bc2dd66426f6d24
wenderf5 <wenderfabianoalves@gmail.com>
2023-11-10 19:20:55 -0300

    1.4

--- script/script.js
+++ script/script.js
@@ -21,6 +21,7 @@ document.getElementById("btnlupa").addEventListener("click", function () {
         imgprincipal.style.display = "block";
         cidade1.style.display = "none";
         bloco2.style.display = "none";
+        img0.style.display = "none"
       }
       else if (erro.style.display == "block") {
         erro.style.display = "none";
@@ -35,6 +36,7 @@ document.getElementById("btnlupa").addEventListener("click", function () {
       let climaKelvin = data.main.temp;
       let climaKelvin1 = parseInt(climaKelvin);
       const temperaturaCelsius = climaKelvin1 - 273;
+      const nuvens = data.weather[0].main;
 
       cidade1.innerHTML = data.name;
       clima.innerHTML = temperaturaCelsius;
@@ -42,6 +44,31 @@ document.getElementById("btnlupa").addEventListener("click", function () {
       vento.innerHTML = data.wind.speed;
       codigoPais = data.sys.country;
       bandeira.src = `https://flagsapi.com/${codigoPais}/flat/32.png`;
+
+      if (nuvens == "Clear") {
+        const texto1 = document.getElementById("texto1");
+        texto1.innerHTML = "Ensolarado"
+      }
+      if (nuvens == "Rain") {
+        const texto1 = document.getElementById("texto1");
+        texto1.innerHTML = "Chuvoso"
+      }
+      if (nuvens == "Clouds") {
+        const texto1 = document.getElementById("texto1");
+        texto1.innerHTML = "Nublado"
+      }
+      if (nuvens == "Clear") {
+        const img1 = document.getElementById("img1");
+        img1.src = "img/Ensolarado.png"
+      }
+      if (nuvens == "Rain") {
+        const img1 = document.getElementById("img1");
+        img1.src = "img/Chuva.png"
+      }
+      if (nuvens == "Clouds") {
+        const img1 = document.getElementById("img1");
+        img1.src = "img/Nublado.png"
+      }
     })
     .catch(error => {
       console.error('Erro:', error);
@@ -49,120 +76,91 @@ document.getElementById("btnlupa").addEventListener("click", function () {
 
   function ax1() {
     let cidade1 = document.getElementById("cidade1");
-    let bloco2 = document.getElementById("bloco2");
+    let bloco4 = document.getElementById("bloco4");
     let imgprincipal = document.getElementById("imgprincipal");
+    let img0 = document.getElementById("img0")
 
     cidade1.style.display = "flex";
-    bloco2.style.display = "flex";
+    bloco4.style.display = "flex";
     imgprincipal.style.display = "none";
+    img0.style.display = "flex"
+    bloco2.style.display = "flex";
+
   }
   ax1()
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${ApiKey}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      data.list.forEach(element => {
-        const tempo1 = element.main.temp;
-        const data1 = element.dt_txt;
-        const clima1 = element.weather[0].main;
-        meuArrayTempo.push(tempo1)
-        meuArrayData.push(data1)
-        meuArrayNuvens.push(clima1)
-        console.log(data)
-        console.log(meuArrayNuvens)
-        ax10()
-      });
-
-    })
-    .catch(error => {
-      console.error('Erro:', error);
-    })
-
-  function ax10() {
-
-    const graus = meuArrayTempo.map(datatamp => {
-      return Math.floor(datatamp - 273);
-    });
-
-    const item1 = graus[0]
-    const item2 = graus[1]
-    const item3 = graus[2]
-    const item4 = graus[3]
-    const item5 = graus[4]
-    const item6 = graus[5]
-    const item7 = graus[6]
-    const item8 = graus[7]
-
-    const horasEMinutos = meuArrayData.map(timestamp => {
-      const partes = timestamp.split(" ")[1];
-      const [hora, minuto] = partes.split(":");
-      return `${hora}:${minuto}`;
-    });
-
-
-
-    const item1d = horasEMinutos[0]
-    const item2d = horasEMinutos[1]
-    const item3d = horasEMinutos[2]
-    const item4d = horasEMinutos[3]
-    const item5d = horasEMinutos[4]
-    const item6d = horasEMinutos[5]
-    const item7d = horasEMinutos[6]
-    const item8d = horasEMinutos[7]
-
-    let hr1 = document.getElementById("0");
-    hr1.innerHTML = item1;
-    let hr2 = document.getElementById("1");
-    hr2.innerHTML = item2;
-    let hr3 = document.getElementById("2");
-    hr3.innerHTML = item3;
-    let hr4 = document.getElementById("3");
-    hr4.innerHTML = item4;
-    let hr5 = document.getElementById("4");
-    hr5.innerHTML = item5;
-    let hr6 = document.getElementById("5");
-    hr6.innerHTML = item6;
-    let hr7 = document.getElementById("6");
-    hr7.innerHTML = item7;
-    let hr8 = document.getElementById("7");
-    hr8.innerHTML = item8;
-
-    let hr1d = document.getElementById("0d");
-    hr1d.innerHTML = item1d;
-    let hr2d = document.getElementById("1d");
-    hr2d.innerHTML = item2d;
-    let hr3d = document.getElementById("2d");
-    hr3d.innerHTML = item3d;
-    let hr4d = document.getElementById("3d");
-    hr4d.innerHTML = item4d;
-    let hr5d = document.getElementById("4d");
-    hr5d.innerHTML = item5d;
-    let hr6d = document.getElementById("5d");
-    hr6d.innerHTML = item6d;
-    let hr7d = document.getElementById("6d");
-    hr7d.innerHTML = item7d;
-    let hr8d = document.getElementById("7d");
-    hr8d.innerHTML = item8d;
-  }
-
-  const i1 = document.getElementById("1i");
-  const i2 = document.getElementById("2i");
-  const i3 = document.getElementById("3i");
-  const i4 = document.getElementById("4i");
-  const i5 = document.getElementById("5i");
-  const i6 = document.getElementById("6i");
-  const i7 = document.getElementById("7i");
-  const i8 = document.getElementById("8i");
-
-  const item1i = meuArrayNuvens[0]
-  const item2i = meuArrayNuvens[1]
-  const item3i = meuArrayNuvens[2]
-  const item4i = meuArrayNuvens[3]
-  const item5i = meuArrayNuvens[4]
-  const item6i = meuArrayNuvens[5]
-  const item7i = meuArrayNuvens[6]
-  const item8i = meuArrayNuvens[7]
-
 });
 
+
+let latitude;
+let longitude;
+
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+
+      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    },
+    function (error) {
+      console.error(`Erro ao obter a localização: ${error.message}`);
+    }
+  );
+} else {
+  console.error("Geolocalização não suportada neste navegador");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  if (latitude !== undefined && longitude !== undefined) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${ApiKey}`)
+      .then(response => response.json())
+      .then(data => {
+          let clima = document.getElementById("clima");
+          let umidade = document.getElementById("umidade");
+          let vento = document.getElementById("vento");
+          let cidade1 = document.getElementById("cidade");
+          let climaKelvin = data.main.temp;
+          let climaKelvin1 = parseInt(climaKelvin);
+          const temperaturaCelsius = climaKelvin1 - 273;
+          const nuvens = data.weather[0].main;
+
+          cidade1.innerHTML = data.name;
+          clima.innerHTML = temperaturaCelsius;
+          umidade.innerHTML = data.main.humidity;
+          vento.innerHTML = data.wind.speed;
+          codigoPais = data.sys.country;
+          bandeira.src = `https://flagsapi.com/${codigoPais}/flat/32.png`;
+
+          if (nuvens == "Clear") {
+            const texto1 = document.getElementById("texto1");
+            texto1.innerHTML = "Ensolarado"
+          }
+          if (nuvens == "Rain") {
+            const texto1 = document.getElementById("texto1");
+            texto1.innerHTML = "Chuvoso"
+          }
+          if (nuvens == "Clouds") {
+            const texto1 = document.getElementById("texto1");
+            texto1.innerHTML = "Nublado"
+          }
+          if (nuvens == "Clear") {
+            const img1 = document.getElementById("img1");
+            img1.src = "img/Ensolarado.png"
+          }
+          if (nuvens == "Rain") {
+            const img1 = document.getElementById("img1");
+            img1.src = "img/Chuva.png"
+          }
+          if (nuvens == "Clouds") {
+            const img1 = document.getElementById("img1");
+            img1.src = "img/Nublado.png"
+          }
+          console.log(data)
+      })
+      .catch(error => {
+        console.error('Erro na solicitação ao OpenWeatherMap:', error);
+      });
+  } else {
+    console.error('Latitude ou longitude não definidas.');
+  }
+});
